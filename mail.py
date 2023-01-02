959a63d00f33c81ac1db1e5da68063d7a7fec613
Osmancan Akagündüz <83430594+osmancan23@users.noreply.github.com>
2023-01-02 18:31:44 +0300

    changed mail file

--- mail.py
+++ mail.py
@@ -1,32 +1,26 @@
 import smtplib
 from email.message import EmailMessage
 
-# Outlook SMTP sunucusunun adresini ve giriş bilgilerini belirtin
 outlook_server = 'smtp-mail.outlook.com'
 outlook_user = 'bsgproje1@outlook.com'
 outlook_password = 'bsgbsg2323'
-
+server_port = 587
+to_User= 'bsgproje1@outlook.com'
 
 def sendMail(message):
 
-    # E-posta mesajını oluşturun
     msg = EmailMessage()
     msg.set_content(message)
     msg['Subject'] = 'Test Mesajı'
     msg['From'] = outlook_user
-    msg['To'] = 'bsgproje1@outlook.com'
+    msg['To'] = to_User
 
-    # SMTP sunucusunu başlatın
-    server = smtplib.SMTP(outlook_server, 587)
+    server = smtplib.SMTP(outlook_server, server_port)
 
-    # Sunucuya bağlanın (güvenliği artırmak için SSL kullanabilirsiniz)
     server.starttls()
 
-    # Sunucuya giriş yapın
     server.login(outlook_user, outlook_password)
 
-    # E-postayı gönderin
     server.send_message(msg)
 
-    # Sunucudan çıkış yapın
     server.quit()
